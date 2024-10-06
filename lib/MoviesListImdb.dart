baa918ad2d6c4bf51ab71d4ae38347b62f429208
kacem <kacemmeherzi52@gmail.com>
2024-10-06 22:37:07 +0100

    movie list from imbd api  added

---
+++ lib/MoviesListImdb.dart
@@ -0,0 +1,19 @@
+import 'package:app/movie.dart';
+import 'package:flutter/material.dart';
+
+class Movieslistimdb extends StatefulWidget {
+  const Movieslistimdb({super.key});
+
+  @override
+  State<Movieslistimdb> createState() => _MovieslistimdbState();
+}
+
+class _MovieslistimdbState extends State<Movieslistimdb> {
+
+  List<Movie> movies = [] ;
+  @override
+  Widget build(BuildContext context) {
+    
+    return const Placeholder();
+  }
+}
