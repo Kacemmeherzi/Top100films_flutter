ea15bbb753c30a902d7f8d9a8c75d39548f568e6
kacem <kacemmeherzi52@gmail.com>
2024-10-06 23:28:03 +0100

    main updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:app/MoviesList.dart';
+import 'package:app/MoviesListImdb.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  Movielist(),
+      home:  Movieslistimdb(),
     );
   }
 }
