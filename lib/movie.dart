9408148bfeabec910d8635ec0a381af2288b9520
kacem <kacemmeherzi52@gmail.com>
2024-10-06 23:28:23 +0100

    movie model updated

--- lib/movie.dart
+++ lib/movie.dart
@@ -1,21 +1,25 @@
 class Movie {
-  final String name;
-  final String imageUrl;
+  final int rank;
+  final String title;
   final String description;
+  final String image;
+  
 
   Movie({
-    required this.name,
-    required this.imageUrl,
+    required this.rank,
+    required this.title,
     required this.description,
+    required this.image,
+  
   });
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
-      name: json['title'],
-      imageUrl: json['big_image'] != null 
-          ? 'https://image.tmdb.org/t/p/w500${json['poster_path']}' // Base URL for images
-          : '',
+      rank: json['rank'],
+      title: json['title'],
       description: json['description'],
+      image: json['image'],
+    
     );
   }
 }
