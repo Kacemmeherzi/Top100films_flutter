ae84a497d77006d112701a87ea0f5c321a7d9201
kacem <kacemmeherzi52@gmail.com>
2024-10-06 22:39:47 +0100

    ferchmovies method added

--- lib/MoviesListImdb.dart
+++ lib/MoviesListImdb.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:app/movie.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Movieslistimdb extends StatefulWidget {
   const Movieslistimdb({super.key});
@@ -11,9 +14,28 @@ class Movieslistimdb extends StatefulWidget {
 class _MovieslistimdbState extends State<Movieslistimdb> {
 
   List<Movie> movies = [] ;
+  Future<List<Movie>> fetchMovies() async {
+    final String apiKey = '771b450204msha3d9833daf12e77p10263bjsnbd0aafc1554f'; 
+    final String baseUrl = 'https://imdb-top-100-movies.p.rapidapi.com/';
+    final apiheaders = {
+       "x-rapidapi-host" :  "imdb-top-100-movies.p.rapidapi.com" , 
+       "x-rapidapi-key" : apiKey 
+
+    };
+    final response = await http.get(Uri.parse('$baseUrl?api_key=$apiKey'),headers: apiheaders);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> json = jsonDecode(response.body);
+      final List<dynamic> results = json['results'];
+
+      return results.map((movie) => Movie.fromJson(movie)).toList();
+    } else {
+      throw Exception('Failed to load movies');
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    
+
     return const Placeholder();
   }
 }
