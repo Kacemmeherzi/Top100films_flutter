a914eea47e412cbca92208b5115d2483dff489f3
kacem <kacemmeherzi52@gmail.com>
2024-10-06 23:29:13 +0100

    fixed

--- lib/MoviesList.dart
+++ lib/MoviesList.dart
@@ -5,18 +5,21 @@ import 'package:flutter/material.dart';
 class Movielist extends StatelessWidget {
       final List<Movie> movies = [
   Movie(
-    name: 'Inception',
-    imageUrl: 'https://example.com/inception.jpg',
+    rank:  1,
+    title: 'Inception',
+    image: 'https://example.com/inception.jpg',
     description: 'A mind-bending thriller about dreams within dreams.',
   ),
   Movie(
-    name: 'The Dark Knight',
-    imageUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.primevideo.com%2F-%2Ffr%2Fdetail%2FThe-Dark-Knight%2F0RV4BHZGQV7WWQYA2USRW1B82H&psig=AOvVaw3ahvaUcLq3jgUFtt0y49cL&ust=1727950291643000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMDx_Me674gDFQAAAAAdAAAAABAE',
+    rank: 2,
+    title: 'The Dark Knight',
+    image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.primevideo.com%2F-%2Ffr%2Fdetail%2FThe-Dark-Knight%2F0RV4BHZGQV7WWQYA2USRW1B82H&psig=AOvVaw3ahvaUcLq3jgUFtt0y49cL&ust=1727950291643000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMDx_Me674gDFQAAAAAdAAAAABAE',
     description: 'Batman faces his ultimate nemesis, the Joker, in this gritty crime drama.',
   ),
   Movie(
-    name: 'Interstellar',
-    imageUrl: 'https://example.com/interstellar.jpg',
+    rank: 3,
+    title: 'Interstellar',
+    image: 'https://example.com/interstellar.jpg',
     description: 'A team of explorers travels through a wormhole in') ] ;
    Movielist({super.key});
 
@@ -34,9 +37,9 @@ class Movielist extends StatelessWidget {
                 child: ListTile(
 
                   leading: ClipRRect( borderRadius: BorderRadius.circular(7),
-                    child: Image.network(movie.imageUrl),
+                    child: Image.network(movie.image),
                     )  , 
-                  title: Text(movie.name),
+                  title: Text(movie.title),
                   subtitle: Text(movie.description),
                   onTap: (){Navigator.push(
                                context,
